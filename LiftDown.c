92e2cd6742123126047c29dd0029740af1d4c169
aguerrero15 <student@Mine.local>
2013-11-12 16:31:09 -0600

    Adding in the code

---
+++ LiftDown.c
@@ -0,0 +1,22 @@
+#pragma config(Hubs,  S1, HTMotor,  HTServo,  HTMotor,  none)
+#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     motorF,        tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C3_2,     motorG,        tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S1_C2_1,    servo1,               tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_2,    servo2,               tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_3,    servo3,               tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_4,    servo4,               tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S1_C2_6,     c servo6,               tServoNone)
+
+#include "JoystickDriver.c"
+task main()
+{
+	while(true)
+	{
+		motor[motorF] = -50;
+		motor[motorG] = -50;
+		wait1Msec(1);
+	}
+}
